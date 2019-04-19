9e1d6637dd502f2fc620291205c27b77eaaf73d8
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-04-19 16:39:59 -0400

    stop with the installer. up to v0.24.1. seedlings of windows support.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
-VER=0.23.2
+VER=0.24.1
 MIRROR=https://github.com/bazelbuild/bazel/releases/download/$VER
 
 dl()
 {
     OS=$1
     PLATFORM=$2
-    SUFFIX=${3:-}
-    URL=$MIRROR/bazel-$VER-installer-$OS-$PLATFORM.sh.sha256
+    EXESUFFIX=${3:-}
+    URL=$MIRROR/bazel-$VER-$OS-$PLATFORM$EXESUFFIX.sha256
     curl -SsL $URL
 }
 
 dl linux x86_64
 dl darwin x86_64
+dl windows x86_64 .exe
 
