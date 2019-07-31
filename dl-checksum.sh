a29339a53f0b79990da56ed037b2071f63e5ae4e
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-07-31 07:50:33 -0500

    up to v0.28.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=0.27.0
+VER=0.28.0
 MIRROR=https://github.com/bazelbuild/bazel/releases/download/$VER
 
 dl()
@@ -8,11 +8,11 @@ dl()
     PLATFORM=$2
     EXESUFFIX=${3:-}
     URL=$MIRROR/bazel-$VER-$OS-$PLATFORM$EXESUFFIX.sha256
-    printf "  # %s\n" $URL
-    printf "  %s-%s: sha256:%s\n" $OS $PLATFORM `curl -SsL $URL | awk '{print $1}'`
+    printf "    # %s\n" $URL
+    printf "    %s-%s: sha256:%s\n" $OS $PLATFORM `curl -SsL $URL | awk '{print $1}'`
 }
 
-printf "'%s':\n" $VER
+printf "  '%s':\n" $VER
 dl linux x86_64
 dl darwin x86_64
 dl windows x86_64 .exe
