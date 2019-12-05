3f434a79fd4d4c57b24f529e19bf57b38aeba35a
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-05 00:02:33 -0500

    up to v1.2.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
-VER=0.29.1
-MIRROR=https://github.com/bazelbuild/bazel/releases/download/$VER
+MIRROR=https://github.com/bazelbuild/bazel/releases/download
 
 dl()
 {
-    local os=$1
-    local arch=$2
-    local exesuffix=${3:-}
-    local url=$MIRROR/bazel-$VER-$os-$arch$exesuffix.sha256
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local exesuffix=${4:-}
+    local platform="${os}-${arch}"
+    local url=$MIRROR/$ver/bazel-$ver-$platform${exesuffix}.sha256
     printf "    # %s\n" $url
     printf "    %s-%s: sha256:%s\n" $os $arch `curl -SsL $url | awk '{print $1}'`
 }
 
-printf "  '%s':\n" $VER
-dl linux x86_64
-dl darwin x86_64
-dl windows x86_64 .exe
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver linux x86_64
+    dl $ver darwin x86_64
+    dl $ver windows x86_64 .exe
+}
 
+dl_ver ${1:-1.2.1}
