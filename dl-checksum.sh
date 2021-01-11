cbf878f43bba2f45f470fd7bf3022b49a6261231
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-01-11 18:45:22 -0500

    drop role dep on andrewrothstein.openjdk. default to install w jdk. support install w/o jdk.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -4,21 +4,35 @@ MIRROR=https://github.com/bazelbuild/bazel/releases/download
 dl()
 {
     local ver=$1
-    local os=$2
-    local arch=$3
-    local exesuffix=${4:-}
+    local app=$2
+    local os=$3
+    local arch=$4
+    local exesuffix=${5:-}
     local platform="${os}-${arch}"
-    local url=$MIRROR/$ver/bazel-$ver-$platform${exesuffix}.sha256
+    local url=$MIRROR/$ver/$app-$ver-$platform${exesuffix}.sha256
     printf "    # %s\n" $url
     printf "    %s-%s: sha256:%s\n" $os $arch `curl -SsL $url | awk '{print $1}'`
 }
 
-dl_ver() {
+dl_bazel() {
     local ver=$1
+    local app=bazel
+    printf "%s_checksums:\n" $app
     printf "  '%s':\n" $ver
-    dl $ver linux x86_64
-    dl $ver darwin x86_64
-    dl $ver windows x86_64 .exe
+    dl $ver $app linux x86_64
+    dl $ver $app darwin x86_64
+    dl $ver $app windows x86_64 .exe
 }
 
-dl_ver ${1:-3.7.2}
+dl_bazel_nojdk() {
+    local ver=$1
+    local app=bazel_nojdk
+    printf "%s_checksums:\n" $app
+    printf "  '%s':\n" $ver
+    dl $ver $app linux x86_64
+    dl $ver $app darwin x86_64
+}
+
+VER=${1:-3.7.2}
+dl_bazel $VER
+dl_bazel_nojdk $VER
